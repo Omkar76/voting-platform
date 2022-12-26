4ac62e6bb749eea071526e4070db280d0f99622c
Your Name <you@example.com>
2022-12-26 12:45:54 +0530

    Generate models with sequelize-cli and add associations

---
+++ db/models/question.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Question extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Question.belongsTo(models.Election, {
+        foreignKey : 'electionId'
+      });
+
+      Question.hasMany(models.Option, {
+        foreignKey : 'questionId'
+      });
+    }
+  }
+  Question.init({
+    title: DataTypes.STRING,
+    description: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Question',
+  });
+  return Question;
+};
