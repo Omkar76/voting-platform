fca8f5693cdad429caf9c458fe5c91d5f7c63aa3
Your Name <you@example.com>
2023-01-01 00:18:24 +0530

    Make async, fix typo in params

--- middleware/checkElectionOwnership.js
+++ middleware/checkElectionOwnership.js
@@ -1,27 +1,37 @@
-function checkElectionOwnership(req, res, next) {
-  // Get the user's id from the request
-  const userId = req.user.id;
+const { Election } = require('../db/models');
 
-  // Find the Election record
-  Election.findOne({
-    where: {
-      id: req.params.electionId,
-      adminId: userId
+module.exports = async function (req, res, next) {
+  // Get the user's id from the request
+  const adminId = req.user.id;
+  const electionId = parseInt(req.params.eid);
+
+  try {
+    if (isNaN(electionId)) {
+      throw "Invalid election";
     }
-  }).then(election => {
+
+    // Find the Election record
+    const election = await Election.findOne({
+      where: {
+        id: electionId,
+        adminId
+      }
+    });
+
     if (!election) {
       return res.status(403).json({
         success: false,
-        message: 'You do not have permission to access this resource'
+        message: "Election doesn't exist or you don't have permission"
       });
     }
-
     // Allow the user to access the resource
     next();
-  }).catch(error => {
-    res.status(500).json({
+  }
+  catch (error) {
+    console.error(error);
+    return res.status(400).json({
       success: false,
       message: error.message
     });
-  });
+  }
 }
