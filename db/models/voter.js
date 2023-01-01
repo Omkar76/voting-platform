cbc20e027ed846d8659a4aeece32e9ab7ab3e957
Your Name <you@example.com>
2023-01-01 23:04:51 +0530

    Add voters

---
+++ db/models/voter.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Voter extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Voter.belongsTo(models.Election, { foreignKey: "electionId" });
+    }
+
+    static addVoter(electionId, voter){
+      Voter.create({...voter, electionId});
+    }
+  }
+  Voter.init({
+    username: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Voter',
+  });
+  return Voter;
+};
