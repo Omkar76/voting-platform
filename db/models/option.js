46d72c4ad984d509b28dcd4fa27ac66c60d5a2e2
Onkar Jadhav <omjadhav2610@gmail.com>
2023-01-08 16:17:02 +0530

    Generate models with sequelize-cli and add associations

---
+++ db/models/option.js
@@ -0,0 +1,23 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Option extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Option.belongsTo(models.Question, { foreignKey: 'questionId' })
+    }
+  }
+  Option.init({
+    text: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Option',
+  });
+  return Option;
+};
