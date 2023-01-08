0674b69444d00c494df2de7c282853bf426a971e
Onkar Jadhav <omjadhav2610@gmail.com>
2023-01-08 16:17:02 +0530

    Change database names

--- db/config.js
+++ db/config.js
@@ -2,21 +2,21 @@ module.exports = {
   "development": {
     "username": "postgres",
     "password": "postgres",
-    "database": "database_development",
+    "database": "voting_dev",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "test": {
     "username": "postgres",
     "password": "postgres",
-    "database": "database_test",
+    "database": "voting_test",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "production": {
     "username": "postgres",
     "password": "postgres",
-    "database": "database_production",
+    "database": "voting_prod",
     "host": "127.0.0.1",
     "dialect": "postgres"
   }
