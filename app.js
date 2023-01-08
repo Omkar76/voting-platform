f42615fcbe661805d1095cdfe486fc1e491b9a13
Onkar Jadhav <omjadhav2610@gmail.com>
2023-01-08 16:17:02 +0530

    Elections, Questions and Options creation functionality

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { Election, Question } = require('./db/models');
+const { Election, Question, Option } = require('./db/models');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
     res.send("Hello!");
 });
@@ -34,11 +35,33 @@ app.put('/elections/:id', async (req, res) => {
 
 app.get('/elections/:id/questions', async (req, res)=>{
     console.log(req.params.id);
-    const election = await Election.findByPk(req.params.id);
+    const election = await Election.findByPk(req.params.id, {include : [{model : Question, as : 'questions'}]});
+
     console.log(election);
-    const questions = await election.getQuestions();
-    console.log(questions);
+    // const questions = await election.getQuestions();
+    // console.log(questions, "questions bro");
     res.render("questions", {election});
 })
+
+
+app.post('/elections/:eid/questions/', async (req, res)=>{
+    const question = await Question.addQuestion(req.params.eid, req.body);
+    res.json(question);
+})
+
+app.get('/elections/:eid/questions/:qid/options', async (req, res)=>{
+    // console.log(req.params.id);
+    const question = await Question.findByPk(req.params.qid, {include : [{model : Option, as : 'options'}]});
+
+    console.log(question);
+    res.render("options", {question});
+})
+
+app.post('/elections/:eid/questions/:qid/options/', async (req, res)=>{
+    const option = await Option.addOption(req.params.qid, req.body);
+    console.log(option)
+    res.json(option);
+})
+
 
 module.exports = app;
